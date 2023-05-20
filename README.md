41b762e9fec2de9eae5c0c6a305f5434f286132d
jacksmedia <alex@j4cks.com>
2023-05-19 17:34:19 -0700

    :tada: 1st 1

---
+++ README.md
@@ -0,0 +1,50 @@
+# HRI app, a Jacks.Media tool for Combased.io
+
+The result of this code is deployed here []().
+
+This webapp provides calculations of metadata based upon wallet addresses on the MultiversX blockchain.
+
+Features:
+- input wallet address, receive HRI score (custom logic)
+- data in table form to explain HRI scores
+- outlinks to client resources
+
+This website is built using [Docusaurus 2](https://docusaurus.io/), a modern static website generator.
+
+### Installation
+
+```
+$ yarn
+```
+
+### Local Development
+
+```
+$ yarn start
+```
+
+This command starts a local development server and opens up a browser window. Most changes are reflected live without having to restart the server.
+
+### Build
+
+```
+$ yarn build
+```
+
+This command generates static content into the `build` directory and can be served using any static contents hosting service.
+
+### Deployment
+
+Using SSH:
+
+```
+$ USE_SSH=true yarn deploy
+```
+
+Not using SSH:
+
+```
+$ GIT_USER=<Your GitHub username> yarn deploy
+```
+
+If you are using GitHub pages for hosting, this command is a convenient way to build the website and push to the `gh-pages` branch.
---
+++ src/components/HomepageFeatures/index.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './styles.module.css';
+
+const FeatureList = [
+  {
+    title: 'Combased',
+    Svg: require('@site/static/img/combased0.svg').default,
+    description: (
+      <>
+        The blockchain SaaS company that launched on MultiversX &
+        continues to shape its future. Fluent in Polygon & ETH & Shopify, too 🦾
+      </>
+    ),
+  },
+  {
+    title: 'ComVerse',
+    Svg: require('@site/static/img/comverse.svg').default,
+    description: (
+      <>
+        Three genesis NFT collections that show the Combased brand
+        and business evolution across the years. Holders receive regular rewards!
+      </>
+    ),
+  },
+  {
+    title: 'Jewelswap',
+    Svg: require('@site/static/img/jewelswap.svg').default,
+    description: (
+      <>
+        Our flagship product, the only AMM (automated market maker) swap
+        on MultiversX! TVL at 5k EGLD via the 🪄 of DeFi! Loan NFTs for capital!
+      </>
+    ),
+  },
+];
+
+function Feature({Svg, title, description}) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
