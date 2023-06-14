f87c3d59052b10134dc7ac6a70813eb903ff0cfd
jacksmedia <alex@j4cks.com>
2023-06-14 16:31:51 -0700

    😈 daemon script updated

--- localdaemon.sh
+++ localdaemon.sh
@@ -1,7 +1,5 @@
-python3 supermain.py
-sleep 4
-cp HolderHRI.csv src/components/ListJSON/data.json
-sleep 4
+python3 main.py
+sleep 3
 git add .
 git commit -m ':memo: HRI automated update'
 git push
