69624d87633cd7f7dcaf87692a142014af6a09c2
jacksmedia <alex@j4cks.com>
2023-07-17 16:59:18 -0700

    🔧 cloud script adjustment

--- netlify/functions/handler.js
+++ netlify/functions/handler.js
@@ -5,7 +5,8 @@ const { schedule } = require("@netlify/functions");
 const handler = async function(event, context) {
     console.log("Received event:", event);
     console.log('Printing this message every hour. What a world!')
-
+    source localdaemon.sh
+    
     return {
         statusCode: 200,
     };
