8b7b3727a455aaef23b9fbff39fb83b43e4b09fb
jacksmedia <alex@j4cks.com>
2023-07-16 19:10:56 -0700

    🔧 adds functions/ + test netlify cron job

---
+++ netlify/functions/handler.js
@@ -0,0 +1,13 @@
+// YOUR_BASE_DIRECTORY/netlify/functions/handler.js
+
+const { schedule } = require("@netlify/functions");
+
+const handler = async function(event, context) {
+    console.log("Received event:", event);
+
+    return {
+        statusCode: 200,
+    };
+};
+
+exports.handler = schedule("@hourly", handler);
