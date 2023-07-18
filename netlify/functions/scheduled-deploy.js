80b9256e228b0b45e73bfb812539e15b62ca83f3
jacksmedia <alex@j4cks.com>
2023-07-17 17:06:55 -0700

    🔧 cloud script adjustment, build hook added

---
+++ netlify/functions/scheduled-deploy.js
@@ -0,0 +1,20 @@
+const fetch = require('node-fetch')
+
+// This is sample build hook
+const BUILD_HOOK = 'https://api.netlify.com/build_hooks/64b5d77d9e23c6177db3e2bc'
+
+const deployer = async () => {
+  await fetch(BUILD_HOOK, {
+    method: 'POST'
+  }).then((response) => {
+    console.log('Build hook response:', response.json())
+  })
+
+  return {
+    statusCode: 200
+  }
+}
+
+export {
+  deployer
+}
