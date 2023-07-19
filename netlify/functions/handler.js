1f6e44c9da73876c953bf8e1dbb5c73c2c3311c2
jacksmedia <alex@j4cks.com>
2023-07-18 19:44:41 -0700

    🔧 test API function begins

--- netlify/functions/handler.js
+++ netlify/functions/handler.js
@@ -1,12 +1,10 @@
-// YOUR_BASE_DIRECTORY/netlify/functions/handler.js
-
 const { schedule } = require("@netlify/functions");
 
 const handler = async function(event, context) {
     console.log("Received event:", event);
     console.log('Printing this message every hour. What a world!')
     source localdaemon.sh
-    
+
     return {
         statusCode: 200,
     };
