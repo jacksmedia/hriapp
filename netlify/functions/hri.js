150e79618709b3f077f01b171a5b4c7255c4e82f
jacksmedia <alex@j4cks.com>
2023-07-20 14:27:57 -0700

    🛠️ WIP on cloud function to match CSV to API req

--- netlify/functions/hri.js
+++ netlify/functions/hri.js
@@ -2,36 +2,36 @@ const axios = require('axios');
 const Papa = require('papaparse');
 
 exports.handler = async (event, context) => {
-  // Step 1: Accept a variable called "erd"
-  // const erd = event.erd;
-  const { name = "erd" } = event.queryStringParameters;
+  // Step 1: Accept a variable called "erd", to match w Column A
+  // const erd = "erd159mypt4myss3mqrs89ft0hjeacffks2690gq9u3mlh73m9sh0w5s09eqhh";
+  const { erd = "Anonymous" } = event.queryStringParameters;
 
   try {
-    // Step 2: Reference a static URL and import the CSV at this URL
+    // Step 2: Reference a static URL & import the CSV at this URL
     const csvUrl = 'https://raw.githubusercontent.com/jacksmedia/hriapp/main/HolderHRI.csv';
     const response = await axios.get(csvUrl);
 
     // Parse the CSV content using PapaParse
     const parsedCsv = Papa.parse(response.data, { header: true });
 
-    // Step 3: Find and return the value in Column B that matches the "erd" in Column A
-    const matchedRow = parsedCsv.data.find((row) => row['Owner'] === erd);
+    // Step 3: Find row that matches the Owner "erd" in Column A
+    const matchedRow = parsedCsv.data.find((row) => row['Owner'] === ${erd});
 
-    if (matchedRow) {
+    if (matchedRow) { // return the HRI value in Column B
       return {
         statusCode: 200,
         body: JSON.stringify({ result: matchedRow['HRI'] }),
       };
-    } else {
+    } else { // report data not found in CSV
       return {
         statusCode: 404,
         body: JSON.stringify({ error: 'HRI Value not found in the golden CSV.' }),
       };
     }
-  } catch (error) {
+  } catch (error) { // report error
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: 'Internal server error.' }),
+      body: JSON.stringify({ error: 'Internal server error, stay tuned.' }),
     };
   }
 };
