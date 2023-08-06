6d185d2f24373729a713f429a0ee932bcb78bb42
jacksmedia <alex@j4cks.com>
2023-08-06 11:24:24 -0700

    ⛏️ change PORT to another var name

--- appy.js
+++ appy.js
@@ -1,11 +1,11 @@
-const PORT = 80;
+const portal = 80;
 const express = require('express');
 const app = express();
 app.use(express.json());
 
-app.listen(PORT, function(err) {
+app.listen(portal, function(err) {
 	if (err) console.log(err);
-	console.log("Server Listens on PORT:", PORT)
+	console.log("Server Listens on PORT:", portal)
 });
 app.get("/status", (request, response) => {
 	const status = {
