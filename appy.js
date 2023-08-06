7c6643c316b59ac27f40b2748ff99b9f09bcb0be
jacksmedia <alex@j4cks.com>
2023-08-05 19:52:04 -0700

    :wrench: express w port var configd

---
+++ appy.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+app.listen(PORT, () => {
+	console.log("Server Listens on PORT:", port)
+});
+app.get("/status", (request, response) => {
+	const status = {
+		"Status": "Running"
+	};
+	response.send(status);
+});
