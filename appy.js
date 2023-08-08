08770fae8c0d00f37d15f0f3c6f84ce374b45da6
jacksmedia <alex@j4cks.com>
2023-08-08 12:30:41 -0700

    :sparkles: express serves dynamic API endpoint

--- appy.js
+++ appy.js
@@ -1,15 +1,38 @@
-const portal = 80;
 const express = require('express');
-const app = express();
-app.use(express.json());
+const fs = require('fs');
+const csv = require('csv-parser');
 
-app.listen(portal, function(err) {
-	if (err) console.log(err);
-	console.log("Server Listens on PORT:", portal)
+const app = express();
+const PORT = 3000;
+
+const csvFilePath = 'HolderHRI.csv'; // point to the component dir!
+
+// app.use(csv()); // middleware invoked huzzah
+
+// GET http handler
+app.get('/hri/:entry', (req, res) => {
+	const entry = req.params.entry;
+
+	// read, search CSV data
+	const results = [];
+	fs.createReadStream(csvFilePath)
+		.pipe(csv())
+		.on('data', (row) => {
+			if (row['Owner'] === entry) {
+				results.push(row['HRI'])
+			}
+		})
+		.on('end', () => {
+			if (results.length > 0) {
+				res.json({ match: results });
+			} else {
+				res.status(404).json({ error: 'This erd Holder Rank Index was not found.'});
+			}
+		});
 });
-app.get("/status", (request, response) => {
-	const status = {
-		"Status": "Running"
-	};
-	response.send(status);
+
+// start server
+app.listen(PORT, () => {
+	console.log(`Server listens on port ${PORT}`)
 });
+	
